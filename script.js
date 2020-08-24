1a727dd77a90568ed304c02c4b4f5a62ba8cab99
Yuliya <yulia.podd@gmail.com>
2020-08-24 22:40:26 +0300

    corrections with counter

--- script.js
+++ script.js
@@ -10,39 +10,33 @@ var spendings = [];
 
 
 
-function addSpending(){
-  var newInput = document.createElement('input');
-  newInput.setAttribute("type", "number");
-    
- if (newInput) {
-  var obj = {
-      
-      id:counter++
-    }
-  
- }
-     
- newInput.setAttribute("id",obj.id);
-  
-  document.getElementById('spendingForm').append(newInput);
-     spendings.push(obj.id);
-    console.log(spendings);
+function addSpending() {
+    var newInput = document.createElement('input');
+    newInput.setAttribute("type", "text");
 
+
+    newInput.setAttribute("id", 'spending' + counter);
+
+    document.getElementById('spendingForm').append(newInput);
+    spendings.push('spending' + counter);
+    console.log(spendings);
+    counter++;
 }
-function calculate(){
-    spendings.push(spendings.length+1);
+
+function calculate() {
+    spendings.push(spendings.length + 1);
     var totalSpendings = 0;
     console.log(typeof totalSpendings);
     var income = document.getElementById('incomeField').value;
     var spending = document.getElementById('spendingField1').value;
     var taxField = document.getElementById('taxToPay');
-    for (i=1; i< spendings.length; i++){
-        
-       totalSpendings+=parseInt(document.getElementById(i).value); 
-console.log(totalSpendings);
-       
-        }
+    for (i = 1; i < spendings.length; i++) {
 
-    var tax = (income - spending - totalSpendings)*taxRate;
+        totalSpendings += parseInt(document.getElementById('spending' + i).value);
+        console.log(totalSpendings);
+
+    }
+
+    var tax = (income - spending - totalSpendings) * taxRate;
     taxField.innerHTML = tax;
 }
